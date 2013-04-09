3642f0e40c038a5614a77be42de4b1dc1c010a3e
Oskar <oskari@student.chalmers.se>
2013-04-09 19:44:59 +0200

    A basic word counter, output format is still in progress

---
+++ Projekt1/Kod/counter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python 
+import sys
+from collections import *
+
+#All input files must specified from stdin 
+#run for example: find rot_path_to_files | grep .txt$  | ./counter.py
+#note the file english_word_stops.txt must be in the working directory 
+
+s = "" 
+
+try: 
+    for line in sys.stdin: 
+        f = open(line.strip('\n'))
+        s = s + f.read() + " " 
+        f.close()
+except:
+    print "failed to open input files"
+    sys.exit(1)
+
+try: 
+    f = open("english_word_stops.txt")
+    common_words = f.read().split()  
+    f.close() 
+except:
+    print "can't find file english_word_stops.txt"
+    sys.exit(1) 
+
+rm = [str(x) for x in range (0,10)] + [',','.',':','&','-','(',')','$','/','"']
+
+s = filter(lambda x : not x in rm,s) 
+
+ss = s.lower().split() 
+
+c = Counter(ss)  
+
+for elem in common_words:
+    del c[elem]  
+
+output = ""
+
+for elem in c.most_common(len(ss)):
+     output = output + str(elem) + '\n'
+try:
+    f = open('output.txt','w')
+    f.write(output)
+    f.close() 
+except:
+    print "not allowed to write to output.txt"
+    sys.exit(1)

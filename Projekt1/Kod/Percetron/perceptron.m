2dde9a7cdac4f567561f2c08424e413b84e6b121
Oscar Carlsson <ocarlsson3@gmail.com>
2013-04-12 09:31:26 +0200

    My perceptron implem. from ML

---
+++ Projekt1/Kod/Percetron/perceptron.m
@@ -0,0 +1,31 @@
+function [w, iter_count] = perceptron(data,Y)  
+
+
+n = size(data,1);
+d = size(data,2);
+
+%Create the randomized w.
+w = randn(d,1);
+
+%
+%PERCEPTRON ALGORITHM
+%
+
+iter_count = 0;
+not_converged = true;
+
+while not_converged
+      not_converged = false;
+      for i = 1:n
+          if sign(w'*data(i,:)') ~= Y(i,:)
+              %added transpose to get correct dim. 
+              %Change depending on data.
+              
+              w = w + (Y(i) * data(i,:))';
+              not_converged = true;
+          end
+      end
+      iter_count = iter_count +1;
+end
+
+w = w/norm(w);

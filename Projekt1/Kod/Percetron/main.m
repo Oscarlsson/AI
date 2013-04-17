f74c84e42feb2a1481ae8f3c122aecaac11c59cc
John Karlsson <tartrate@gmail.com>
2013-04-17 16:33:44 +0200

    Fix fix

---
+++ Projekt1/Kod/Percetron/main.m
@@ -0,0 +1,6 @@
+load('../data.mat');
+
+classifications = run_perceptron(wordcount, labels_sentiment, wordcount);
+
+disp(sprintf('Classified correctly: %1.2f percent.', ...
+    sum(labels_sentiment == classifications) / length(labels_sentiment)))
--- Projekt1/Kod/Percetron/run_perceptron.m
+++ Projekt1/Kod/Percetron/run_perceptron.m
@@ -1,40 +1,54 @@
 %TODO: fixa plot för att visa hur många iterationer som behövs innan den konvergerar. 
 % träna på allt och testa på allt ger runt 74%
 
-function [percent] = run_perceptron() 
+function [ classifications ] = run_perceptron(training_data, training_labels, test_data) 
 
-load('../data.mat');
-nDocuments = length(wordcount) 
+nTrainingDocuments = length(training_data);
+nTestDocuments = length(test_data);
+
 max_id = 0; 
 
 %find highest word id
-for i = 1 : nDocuments
-   idarray = wordcount{i}.id;
+for i = 1 : nTrainingDocuments
+   idarray = training_data{i}.id;
    max_elem = max(idarray);
    if (max_elem > max_id)
       max_id = max_elem ; 
    end 
 end
-max_id
+max_id;
 
-%initalize data matrix for perceptron
-%update data matrix with cnt, for each document
-data = zeros(nDocuments,max_id); 
-for i = 1 : nDocuments
-    id = wordcount{i}.id; 
-    cnt = wordcount{i}.cnt;
+%initalize TRAINING data matrix for perceptron
+%update TRAINING data matrix with cnt, for each document
+trainingData = zeros(nTrainingDocuments,max_id); 
+for i = 1 : nTrainingDocuments
+    id = training_data{i}.id; 
+    cnt = training_data{i}.cnt;
     for j = 1 : length(id)
-        data(i,id(j)) = cnt(j);
-    end
-       
+        trainingData(i,id(j)) = cnt(j);
+    end   
 end
 
-%training on everthing!
-%pos = categories.pos; 
-pos = 1*(categories.pos == 1) + (-1)*(categories.pos == -1);
-w = perceptron(data, pos);
+%initalize TEST data matrix for perceptron
+%update TEST data matrix with cnt, for each document
+testData = zeros(nTestDocuments,max_id); 
+for i = 1 : nTestDocuments
+    id = test_data{i}.id; 
+    cnt = test_data{i}.cnt;
+    for j = 1 : length(id)
+        testData(i,id(j)) = cnt(j);
+    end   
+end
 
-%testing on everything
-percent = sum(sign((data*w)') == pos)/length(pos);
+% (-1) <- (2)
+% ( 1) <- (1)
+classes = 1*(training_labels == 1) + (-1)*(training_labels == 2);
+w = perceptron(trainingData, classes);
+
+classSigns = sign((testData*w)');
+
+% (-1) -> (2)
+% ( 1) -> (1)
+classifications = 1*(classSigns == 1) + 2*(classSigns == -1);
 
 end 

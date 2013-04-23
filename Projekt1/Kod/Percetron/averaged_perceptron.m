7cb89a56c31df8214ae3d4eb06ac2e7e45524602
Carro <caroline.strandberg90@gmail.com>
2013-04-23 23:02:42 +0200

    fixat average perceptron m plot

---
+++ Projekt1/Kod/Percetron/averaged_perceptron.m
@@ -0,0 +1,43 @@
+%average perceptron
+function [wa] = averaged_perceptron(data, labels, N)
+
+[n, d] = size(trainingData);
+<<<<<<< HEAD
+%N = 50;
+=======
+
+N = 20;
+>>>>>>> 07b0bbf1f3b7a865a89c5d552a56b6bef82157bb
+count = 0;
+
+%Initialize w and wa.
+w = zeros(d,1);
+wa = zeros(d,1);
+
+%
+%PERCEPTRON ALGORITHM
+%
+validationErrors = [];
+trainingErrors = [];
+for j = 1:N %number of iterations
+%    validationErrors = [validationErrors, ...
+%        classification_error(w, validationData, validationLabels)];
+%    trainingErrors = [trainingErrors, ...
+%        classification_error(w, trainingData, trainingLabels)];
+      for i = 1:n
+          if sign((data(i,:)*w)') ~= labels(i) %makes mistake?
+              w = w + (labels(i) * data(i,:))'; %update w
+              average_weight = (N*n - count) / (N*n);
+              wa = wa + average_weight * (labels(i) * data(i,:))';
+          end
+          count = count + 1;
+      end
+end
+
+%hold on
+%    plot(validationErrors, 'r')
+%    plot(trainingErrors, 'b')
+%hold off
+
+wa = wa/norm(wa);
+end
--- Projekt1/Kod/Percetron/averaged_perceptron_validation.m
+++ Projekt1/Kod/Percetron/averaged_perceptron_validation.m
@@ -1,22 +1,28 @@
 %average perceptron
-function [wa] = averaged_perceptron(data, labels, N)
+function [wa] = averaged_perceptron(data, labels)
 
 [trainingData, trainingLabels, validationData, validationLabels] = ...
     training_validation_partition(data, labels, 10);
 
 [n, d] = size(trainingData);
-%N = 50;
+
+N = 20;
 count = 0;
 
-%Initialize w and wa.
+%Create the randomized w.
 w = zeros(d,1);
 wa = zeros(d,1);
 
 %
 %PERCEPTRON ALGORITHM
 %
+validationErrors = [];
+trainingErrors = [];
 for j = 1:N %number of iterations
-    %classification_error(w, validationData, validationLabels);
+% validationErrors = [validationErrors, ...
+% classification_error(w, validationData, validationLabels)];
+% trainingErrors = [trainingErrors, ...
+% classification_error(w, trainingData, trainingLabels)];
       for i = 1:n
           if sign((trainingData(i,:)*w)') ~= trainingLabels(i) %makes mistake?
               w = w + (trainingLabels(i) * trainingData(i,:))'; %update w
@@ -26,6 +32,13 @@ for j = 1:N %number of iterations
           count = count + 1;
       end
 end
+
+%hold on
+% plot(validationErrors, 'r')
+% plot(trainingErrors, 'b')
+%hold off
+
 wa = wa/norm(wa);
 end
+
 

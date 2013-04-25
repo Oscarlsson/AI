9b0c2b91b2b1189635cafb613fc1a73c1bc42212
johnkarlsson <john.karlsson@outlook.com>
2013-04-25 12:49:58 +0200

    Generalized main.m

--- Projekt1/Kod/main.m
+++ Projekt1/Kod/main.m
@@ -1,5 +1,5 @@
 %% This file runs the different algos on some datasets...
-
+clear
 % Init data and paths.
 load data.mat
 addpath('Percetron')
@@ -7,67 +7,25 @@ addpath('Naive Bayes')
 addpath('SVM')
 addpath('K Nearest')
 
+N_PERCEPTRON_ITERATIONS = 25;
+N_AVG_PERCEPTRON_ITERATIONS = 25;
+KNN_K = 20;
 
-labels = {labels_classes, labels_sentiment};
+nWords = max(cellfun(@(x) max(x.id), wordcount));
 
-%LOOP OVER LABELS 
-%for j = 1:size(labels,2)
+algorithms = { ...
+    @(training_data, training_labels, test_data) ...
+        run_perceptron(training_data, training_labels, test_data, N_PERCEPTRON_ITERATIONS), ...
+    @(training_data, training_labels, test_data) ...
+        run_naivebayes(training_data, training_labels, test_data, nWords), ...
+    @(training_data, training_labels, test_data) ...
+        run_average_perceptron(training_data, training_labels, test_data, N_AVG_PERCEPTRON_ITERATIONS), ...
+    @(training_data, training_labels, test_data) ...
+        run_knn(training_data, training_labels, test_data, nWords, KNN_K) ...
+};
 
-%CV-Stuff
-CVP = cvpartition(size(wordcount,2), 'k', 10);
-
-nrAlg = 5;
-timeRun = zeros(nrAlg, CVP.NumTestSets);
-
-% LOOOOOP OVER CV 
-
-for i = 1:CVP.NumTestSets
-
-    % Training data
-    training_data = wordcount(CVP.training(i));
-    training_targets = labels_sentiment(CVP.training(i));
-    test_data = wordcount(CVP.test(i));
-    test_targets = labels_sentiment(CVP.test(i));
-    % Only use a subset of training/test data to decrease runtime...
-    test_data = test_data(1:100);
-    test_targets = test_targets(1:100);
-
-    % PERCEPTRON
-    tic;
-    N = 10;
-    perceptron_classifications = run_perceptron(training_data, training_targets, test_data, N);
-    %nErrors = sum(classifications ~= test_targets)
-    correctFraction_sent_aver = sum(perceptron_classifications == test_targets) / length(test_targets);
-    disp(sprintf('(P) Classified correctly: %1.2f.', correctFraction_sent_aver));
-    timeRun(1,i) = toc;
-
-    %Averaged Perceptron
-    tic;
-    classifications = run_average_perceptron(training_data, training_targets, test_data, N);
-    correctFraction_sent_aver = sum(classifications == test_targets) / length(test_targets);
-    disp(sprintf('(AP) Classified correctly: %1.2f.', correctFraction_sent_aver));
-    timeRun(2,i) = toc;
-
-    % NAIVE BAYES
-    tic;
-    nWords = 2000;
-    classifications = run_naivebayes(training_data, training_targets, test_data, nWords);
-    correctFraction_sent_aver = sum(classifications == test_targets) / length(test_targets);
-    disp(sprintf('(NB) Classified correctly: %1.2f percent.', correctFraction_sent_aver));
-    timeRun(3,i) = toc;
-
-    % K NEAREST NEIG
-    %tic;
-    %k = 5;
-    %classifications = run_knn(training_data, training_targets, test_data, nWords, k);
-    %correctFraction = sum(classifications == test_targets) / length(test_targets);
-    %disp(sprintf('(KNN) Classified correctly: %1.2f.', correctFraction));
-    %timeRun(4,i) = toc;
-
-    % SVM
-    %tic;
-    %classifications = run_svm( training_data, training_targets, test_data, nWords );
-    %correctFraction = sum(classifications == test_targets) / length(test_targets);
-    %disp(sprintf('(SVM) Classified correctly: %1.2f percent.', correctFraction));
-    %timeRun(5,i) = toc
+for algoIndex = 1:size(algorithms,2)
+    % disp('Running');
+    [error, timing] = test_algorithm( algorithms{algoIndex}, wordcount, labels_sentiment, wordcount, labels_sentiment, 1)
+    % disp('Done');
 end
---
+++ Projekt1/Kod/test_algorithm.m
@@ -0,0 +1,37 @@
+function [ error, timing ] = test_algorithm( algorithm_handle, training_dataset, training_dataset_labels, test_dataset, test_dataset_labels, training_dataset_size_fraction )
+%UNTITLED3 Summary of this function goes here
+
+errors = zeros(1,10);
+timings = zeros(1,10);
+
+% 10-fold CV
+CVP = cvpartition(size(training_dataset,2), 'k', 10);
+for i = 1:CVP.NumTestSets
+
+    % Training data
+    training_data = training_dataset(CVP.training(i));
+    training_targets = training_dataset_labels(CVP.training(i));
+    test_data = test_dataset(CVP.test(i));
+    test_targets = test_dataset_labels(CVP.test(i));
+    
+    % DEBUG
+    % Only use a subset of training/test data to decrease runtime...
+    test_data = test_data(1:100);
+    test_targets = test_targets(1:100);
+    
+    training_data_size = ceil(training_dataset_size_fraction * size(training_data, 2));
+    tic;
+    classifications = feval(algorithm_handle, training_data(1:training_data_size), ...
+        training_targets(1:training_data_size), test_data);
+    
+    timings(i) = toc;
+    errors(i) = sum(classifications ~= test_targets) / length(test_targets);
+     
+    [timings(i), errors(i)]
+end
+
+timing = mean(timings);
+error = mean(errors);
+
+end
+

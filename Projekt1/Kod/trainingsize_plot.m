1c7aa3a5eedb574fc2f7cbaf355b743c902f87f5
Oscar Carlsson <ocarlsson3@gmail.com>
2013-04-27 12:08:22 +0200

    Plots and backup-files. Woho!

--- Projekt1/Kod/trainingsize_plot.m
+++ Projekt1/Kod/trainingsize_plot.m
@@ -33,29 +33,34 @@ algorithms = { ...
 nLabels = 6;
 nrAlgorithms = size(algorithms,2);
 
-tss_values = linspace(0.2,1,5);
+tss_values = linspace(0.2,1,10);
 errorAlg = zeros(nrAlgorithms,1);
-outputArray = zeros(length(tss_values), nrAlgorithms);
+timingAlg= zeros(nrAlgorithms,1);
+
+outputArrayAlg = zeros(length(tss_values), nrAlgorithms);
+outputArrayTiming = zeros(length(tss_values), nrAlgorithms);
 
 nWords = max(cellfun(@(x) max(x.id), wordcount));
 
 for tss_index = 1:length(tss_values)
     tss_value = tss_values(tss_index)
-    
+             %save('backup_trainingsize.mat', 'outputArrayAlg', 'tss_index', 'outputArrayTiming');
+
     for algoIndex = 1:nrAlgorithms
-        
+      % save('backup_trainingsize.mat', 'outputArrayAlg', 'tss_index', 'outputArrayTiming');   
         for i = 1:nLabels
            category_data = wordcount(labels_classes == i);
            category_labels = labels_sentiment(labels_classes == i);
            [error, timing] = test_algorithm( algorithms{algoIndex}, category_data, category_labels, category_data, category_labels, tss_value, nWords);
-        
+           timingAlg(i) = timing;
            errorAlg(i) = error;
         end
         
         %Average over all labels in-domain
-        outputArray(tss_index, algoIndex) = mean(errorAlg);
-        
-        save('backup_trainingsize.mat', 'outputArray', 'tss_index');
+        outputArrayAlg(tss_index, algoIndex) = mean(errorAlg);
+        outputArrayTiming(tss_index, algoIndex) = mean(timingAlg);
+
+        save('backup_trainingsize.mat', 'outputArrayAlg', 'tss_index', 'outputArrayTiming');
     end
 end
 

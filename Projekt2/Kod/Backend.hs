6a24b1c7341f2212c23bf2c0be8728a6e601afe0
John Karlsson <tartrate@gmail.com>
2013-05-15 13:54:12 +0200

    Removed unneccessary stuff from Backend

--- Projekt2/Kod/Backend.hs
+++ Projekt2/Kod/Backend.hs
@@ -15,46 +15,6 @@ type Indexes = M.Map Block Int
 data World = W {holding :: Maybe Block , ground :: Ground, indexes :: Indexes} 
     deriving (Show)
 
-data Instruction = Drop Int | Pick Int deriving (Show, Eq)
-instance Ord Instruction where
-	Pick l1 `compare` Pick l2 = l1 `compare` l2
-	Drop l1 `compare` Drop l2 = l1 `compare` l2
-	Pick l1 `compare` Drop l2 = l1 `compare` l2
-	Drop l1 `compare` Pick l2 = l1 `compare` l2
-
---------------------------------------------------------------------------------
-
-validInstruction :: Instruction -> World -> Bool
-validInstruction i w = case i of 
-				Pick x -> validPickId x w 
-				Drop x -> validDrop x w 
-
-validPickId :: Int -> World -> Bool
-validPickId i w 
-			  | isJust $ holding w = False
-			  | otherwise = case M.lookup i (ground w) of 
-					Nothing		-> False
-					Just []		-> False  
-					Just (x:xs) -> True
-
-validPick :: Block -> World -> Bool 
-validPick b w | isJust $ holding w = False      
-              | otherwise = 
-                case M.lookup b (indexes w) of 
-                 Nothing -> False  
-                 Just i  -> case M.lookup i (ground w) of 
-                                    Nothing     -> False 
-                                    Just (x:xs) -> x == b 
-                                    Just []     -> False  
-
-validDrop :: Int -> World -> Bool
-validDrop i w = case holding w  of 
-                    Nothing -> False 
-                    Just holdBlock  -> case M.lookup i (ground w) of 
-                                         Just []     -> True
-                                         Just (groundBlock:xs) -> groundBlock <= holdBlock
-                                         Nothing     -> False   
-
 --------------------------------------------------------------------------------
 
 findBlockByName :: String -> World -> Maybe Block 
